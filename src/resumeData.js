5fb4d701864b45f4310bce3d2e5b896b424427f4
Davon Larson <davon@Davons-MacBook-Pro.local>
2019-07-11 18:15:18 -0500

    add packages

---
+++ src/resumeData.js
@@ -0,0 +1,117 @@
+let resumeData = {
+    "imagebaseurl":"https://outsidedav.github.io/",
+    "name": "Davon Larson",
+    "role": "not your average UX designer",
+    "linkedinId":"davonlarson",
+    "blah": "blah",
+    "roleDescription": "",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/davonlarson",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/outsidedav",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/davontheweb",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"Don't worry, no one knows how to say it the first time, but I pronounce it Dav-in. I'm a UX designer at IBM, a recent IDM graduate at NYU Tandon School of Engineering. Most of my pre-graduate experience was in Project Management for various digital agencies. As a graduate student, I'm interested in the cross-section of Design and Emerging Technology. As an employee of NYU, I strive to follow Design Thinking in every problem I try and solve.",
+    "address":"San Francisco, California",
+    "website":"davonlarson.com",
+    "education":[
+      {
+        "UniversityName":"New York University",
+        "specialization":"Emerging Technology",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2019",
+        "Achievements":"3.95 GPA"
+      },
+      {
+        "UniversityName":"BYU-Idaho",
+        "specialization":"Communications",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2014",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs (jk)"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Augmenting Urban Planning",
+        "description":"mobileapp",
+        "imgurl":"images/prethesisthum.png",
+        "url": "/thesis"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/VMThumbnail.png"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/worklinkThumbnail.jpg"
+      },
+      {
+        "name":"Augmenting Urban Planning",
+        "description":"mobileapp",
+        "imgurl":"images/prethesisthum.png"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/VMThumbnail.png"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/worklinkThumbnail.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
