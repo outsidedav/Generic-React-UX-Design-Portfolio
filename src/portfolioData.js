167d2479a53c7066a77e297ae1800063e9b7af0d
Davon R Larson <davon@ibm.com>
2021-05-31 16:43:00 -0700

    save

---
+++ src/portfolioData.js
@@ -0,0 +1,55 @@
+let portfolioData = {
+    "imagebaseurl":"https://outsidedav.github.io/",
+    "page-name": "Portfolio Page 1",
+    "role": "ux design",
+    "time":"3 years",
+    "blah": "blah",
+    "roleDescription": "",
+
+    "phase1":[
+      {
+        "head":"This is a really cool project",
+        "subhead":"so cool",
+        "paragraph":"let me tell you about it",
+        "blockquote":"2019",
+        "imgurl":"https://www.wtoc.com/resizer/yph1AWIn8fNZCiFXRE1Gcl5sMoA=/1200x600/arc-anglerfish-arc2-prod-raycom.s3.amazonaws.com/public/YULIKTQXNFFIBJPSYPM3GE4EIE.jpg"
+      },
+      {
+        "head":"This is a really cool project",
+        "subhead":"so cool",
+        "paragraph":"let me tell you about it",
+        "blockquote":"2019",
+        "imgurl":"https://www.wtoc.com/resizer/yph1AWIn8fNZCiFXRE1Gcl5sMoA=/1200x600/arc-anglerfish-arc2-prod-raycom.s3.amazonaws.com/public/YULIKTQXNFFIBJPSYPM3GE4EIE.jpg"
+      }
+    ],
+    "Phase2":[
+      {
+        "head":"This is a really cool project",
+        "subhead":"so cool",
+        "paragraph":"let me tell you about it",
+        "blockquote":"2019",
+        "imgurl":"https://www.wtoc.com/resizer/yph1AWIn8fNZCiFXRE1Gcl5sMoA=/1200x600/arc-anglerfish-arc2-prod-raycom.s3.amazonaws.com/public/YULIKTQXNFFIBJPSYPM3GE4EIE.jpg"
+      },
+      {
+        "head":"This is a really cool project",
+        "subhead":"so cool",
+        "paragraph":"let me tell you about it",
+        "blockquote":"2019",
+        "imgurl":"https://www.wtoc.com/resizer/yph1AWIn8fNZCiFXRE1Gcl5sMoA=/1200x600/arc-anglerfish-arc2-prod-raycom.s3.amazonaws.com/public/YULIKTQXNFFIBJPSYPM3GE4EIE.jpg"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs (jk)"
+      }
+    ],
+  }
+  
+  export default portfolioData
